fa0e5d3cd180efc167089b95cff635bf4d73413c
YefriA <60200890+YefriA@users.noreply.github.com>
2020-01-29 17:27:14 -0500

    Add files via upload

---
+++ JuevesS2A.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jueves1
+{
+    class JuevesS2A
+    {
+        static void Main()
+        {
+            Console.WriteLine("ingrese su salario");
+            int sal = int.Parse(Console.ReadLine());
+
+            if (sal < (2 * 877803))
+            {
+                Console.WriteLine("Tarifa A");
+            }
+            else if (sal < 4 * 877803)
+            {
+                Console.WriteLine("Tarifa B");
+            }
+            else
+                Console.WriteLine("Tarifa C");
+        }
+    }
+}
---
+++ Semana2JuevesB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jueves2
+{
+    class Semana2JuevesB
+    {
+        static void Main()
+        {
+            Console.WriteLine("digite su salario");
+            int sal = int.Parse(Console.ReadLine());
+
+            if (sal < 2 * 877803) {
+                Console.WriteLine("Usted tiene tarifa A y el valor de las cuotas moderadoras es de 3400 pesos");
+            }
+            else if (sal < 5 * 877803) {
+                Console.WriteLine("Usted tiene tarifa B y el valor de las cuotas moderadoras es de 13500 pesos");
+            }
+            else {
+                Console.WriteLine("Usted tiene tarifa C y el valor de las cuotas moderadoras es de 35600 pesos");}
+
+
+        }
+    }
+}
